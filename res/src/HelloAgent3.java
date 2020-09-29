d3d317dd82becb3fc70a9b82d16b38c0c3fee5a9
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-09-29 23:08:47 +0200

    + sm theory about jade api

--- res/src/HelloAgent3.java
+++ res/src/HelloAgent3.java
@@ -1,25 +1,20 @@
-
 public class ExampleAgent extends Agent {
-    @Override
-    protected void setup() {
-        addBehaviour(mainBehaviour());
-    }
+  @Override
+  protected void setup() { addBehaviour(mainBehaviour()); }
 
-    private int rising, falling; // initialised to 0
+  private int rising, falling; // initialised to 0
 
-    @Override
-    private Behaviour risingCount() {
-        return new CyclicBehaviour() {
-            @Override
-            public void action() { log("Hello world! (%d)", round++); } 
-        };
-    }
+  @Override private Behaviour risingCount() {
+    return new CyclicBehaviour() {
+      @Override
+      public void action() { log("Hello world! (%d)", round++); }
+    };
+  }
 
-    @Override
-    private Behaviour fallingCount() {
-        return new CyclicBehaviour() {
-            @Override
-            public void action() { log("Hello world! (%d)", round++); } 
-        };
-    }
+  @Override private Behaviour fallingCount() {
+    return new CyclicBehaviour() {
+      @Override
+      public void action() { log("Hello world! (%d)", round++); }
+    };
+  }
 }
