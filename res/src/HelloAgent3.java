24f620f9126aa9ed7c4cb04cc496a6df19681198
Giovanni <giovanni.ciatto@unibo.it>
2020-09-29 19:10:53 +0200

    wip

---
+++ res/src/HelloAgent3.java
@@ -0,0 +1,25 @@
+
+public class ExampleAgent extends Agent {
+    @Override
+    protected void setup() {
+        addBehaviour(mainBehaviour());
+    }
+
+    private int rising, falling; // initialised to 0
+
+    @Override
+    private Behaviour risingCount() {
+        return new CyclicBehaviour() {
+            @Override
+            public void action() { log("Hello world! (%d)", round++); } 
+        };
+    }
+
+    @Override
+    private Behaviour fallingCount() {
+        return new CyclicBehaviour() {
+            @Override
+            public void action() { log("Hello world! (%d)", round++); } 
+        };
+    }
+}
